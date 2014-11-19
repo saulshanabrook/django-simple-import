133894b29677b619b3c1bdd9736ad1a1be2b6780
Saul Shanabrook <s.shanabrook@gmail.com>
2014-11-19 01:33:04 -0500

    split up tests in travis

--- test.sh
+++ test.sh
@@ -4,11 +4,15 @@
 # DATABASES settings
 # can't overide during tests: https://code.djangoproject.com/ticket/19031
 
-echo
-echo 'RUNNING NOMRAL TESTS'
-echo
-django-admin.py test --noinput --settings=test.settings test.test_simple
-echo
-echo 'RUNNING WITH TENANT SCHEMA TESTS'
-echo
-django-admin.py test --noinput --settings=test.settings_tenant_schemas test.test_simple_tenant_schemas.SimpleTestTenantSchemas
+if ! [ "$TEST_TYPE" = "tenant" ]; then
+    echo
+    echo 'RUNNING NOMRAL TESTS'
+    echo
+    django-admin.py test --noinput --settings=test.settings test.test_simple
+fi;
+if ! [ "$TEST_TYPE" = "normal" ]; then
+    echo
+    echo 'RUNNING WITH TENANT SCHEMA TESTS'
+    echo
+    django-admin.py test --noinput --settings=test.settings_tenant_schemas test.test_simple_tenant_schemas.SimpleTestTenantSchemas
+fi;
